1523c8f66f6c79a6e55f7033734b1c8050b39dda
atharvayew <145331376+atharvayew@users.noreply.github.com>
2024-08-03 21:04:31 +0530

    Add files via upload

---
+++ Prodigy_WD_01/proj1.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('show');
+});
